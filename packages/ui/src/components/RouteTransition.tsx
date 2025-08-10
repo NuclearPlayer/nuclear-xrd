d4d111e3f862c40e3854220458eb2d0a387eafc3
nukeop <12746779+nukeop@users.noreply.github.com>
2025-08-10 19:59:52 +0200

    Fix animated route transition

--- packages/ui/src/components/RouteTransition.tsx
+++ packages/ui/src/components/RouteTransition.tsx
@@ -1,5 +1,6 @@
 import { Transition } from '@headlessui/react';
 import {
+  AnyRouter,
   getRouterContext,
   Outlet,
   RouterContextProvider,
@@ -7,7 +8,44 @@ import {
   useMatches,
 } from '@tanstack/react-router';
 import { cloneDeep } from 'lodash-es';
-import { forwardRef, useContext, useRef } from 'react';
+import { forwardRef, useContext, useEffect, useState } from 'react';
+
+type OutletInstance = {
+  id: string;
+  context: AnyRouter;
+  isLeaving: boolean;
+};
+
+const AnimatedOutlet = forwardRef<
+  HTMLDivElement,
+  {
+    instance: OutletInstance;
+    onExited: () => void;
+  }
+>((props, ref) => {
+  const { instance, onExited } = props;
+
+  return (
+    <Transition
+      show={!instance.isLeaving}
+      appear={true}
+      enter="transition-opacity duration-200 ease-out"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition-opacity duration-150 ease-in"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+      afterLeave={onExited}
+      as="div"
+      ref={ref}
+      className="absolute inset-0 h-full w-full"
+    >
+      <RouterContextProvider router={instance.context}>
+        <Outlet />
+      </RouterContextProvider>
+    </Transition>
+  );
+});
 
 export const RouteTransition = () => {
   const matches = useMatches();
@@ -19,41 +57,53 @@ export const RouteTransition = () => {
 
   const RouterContext = getRouterContext();
   const routerContext = useContext(RouterContext);
-  const renderedContext = useRef(routerContext);
-  const isPresent = useRef(true);
 
-  if (isPresent.current) {
+  const [outlets, setOutlets] = useState<OutletInstance[]>(() => {
     const clone = cloneDeep(routerContext);
     clone.options.context = routerContext.options.context;
-    renderedContext.current = clone;
-  }
+    return [
+      {
+        id: nextMatch.id,
+        context: clone,
+        isLeaving: false,
+      },
+    ];
+  });
 
-  const TransitionContent = forwardRef<HTMLDivElement>((props, ref) => (
-    <div ref={ref} {...props} className="h-full w-full max-h-full">
-      <RouterContextProvider router={renderedContext.current}>
-        <Outlet />
-      </RouterContextProvider>
-    </div>
-  ));
+  useEffect(() => {
+    const currentOutlet = outlets.find((o) => !o.isLeaving);
+    if (!currentOutlet || currentOutlet.id !== nextMatch.id) {
+      setOutlets((prev) =>
+        prev.map((outlet) => ({ ...outlet, isLeaving: true })),
+      );
+
+      const clone = cloneDeep(routerContext);
+      clone.options.context = routerContext.options.context;
+
+      setOutlets((prev) => [
+        ...prev,
+        {
+          id: nextMatch.id,
+          context: clone,
+          isLeaving: false,
+        },
+      ]);
+    }
+  }, [nextMatch.id, routerContext]);
+
+  const handleOutletExited = (instanceId: string) => {
+    setOutlets((prev) => prev.filter((outlet) => outlet.id !== instanceId));
+  };
 
   return (
-    <Transition
-      show={true}
-      appear={true}
-      key={nextMatch.pathname}
-      enter="transition-opacity duration-200 ease-out"
-      enterFrom="opacity-0"
-      enterTo="opacity-100"
-      leave="transition-opacity duration-150 ease-in"
-      leaveFrom="opacity-100"
-      leaveTo="opacity-0"
-      beforeEnter={() => {
-        isPresent.current = true;
-      }}
-      beforeLeave={() => {
-        isPresent.current = false;
-      }}
-      as={TransitionContent}
-    />
+    <div className="relative h-full w-full overflow-hidden">
+      {outlets.map((instance) => (
+        <AnimatedOutlet
+          key={instance.id}
+          instance={instance}
+          onExited={() => handleOutletExited(instance.id)}
+        />
+      ))}
+    </div>
   );
 };
