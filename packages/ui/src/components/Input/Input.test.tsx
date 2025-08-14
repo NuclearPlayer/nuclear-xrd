b9dbcb3e6bde19170eaf2dfdef842d949666a252
nukeop <12746779+nukeop@users.noreply.github.com>
2025-08-14 17:00:12 +0200

    Snapshot tests for input

---
+++ packages/ui/src/components/Input/Input.test.tsx
@@ -0,0 +1,32 @@
+import { render } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import '@nuclearplayer/tailwind-config';
+
+import { Input } from '.';
+
+describe('Input', () => {
+  it('(Snapshot) renders text input with description', () => {
+    const { container } = render(
+      <Input
+        id="input-username"
+        label="Username"
+        placeholder="your-username"
+        description="This is your public display name."
+      />,
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('(Snapshot) renders with error and no description', () => {
+    const { container } = render(
+      <Input
+        id="input-username-error"
+        label="Username"
+        placeholder="your-username"
+        error="Username is already taken"
+      />,
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
