4f0f4831ae7f07bfaf0cd0e12237a573ecad491f
nukeop <12746779+nukeop@users.noreply.github.com>
2025-08-22 00:08:10 +0200

    Codecov vite plugin

--- packages/player/vite.config.ts
+++ packages/player/vite.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest" />
 /// <reference types="vite-plugin-svgr/client" />
+import { codecovVitePlugin } from '@codecov/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackRouter } from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react';
@@ -7,7 +8,17 @@ import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig(async () => ({
-  plugins: [react(), tanstackRouter(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    tanstackRouter(),
+    tailwindcss(),
+    svgr(),
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: 'player',
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
   clearScreen: false,
   server: {
     port: 5173,
