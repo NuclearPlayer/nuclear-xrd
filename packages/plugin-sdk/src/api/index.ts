102d213748cfb0d5fb8d467f765a6288116265be
nukeop <12746779+nukeop@users.noreply.github.com>
2025-08-24 16:18:46 +0200

    Providers host

--- packages/plugin-sdk/src/api/index.ts
+++ packages/plugin-sdk/src/api/index.ts
@@ -1,11 +1,18 @@
+import { ProvidersHost } from '../types/providers';
 import type { SettingsHost } from '../types/settings';
+import { Providers } from './providers';
 import { Settings } from './settings';
 
 export class NuclearAPI {
   readonly Settings: Settings;
+  readonly Providers: Providers;
 
-  constructor(opts?: { settingsHost?: SettingsHost }) {
+  constructor(opts?: {
+    settingsHost?: SettingsHost;
+    providersHost?: ProvidersHost;
+  }) {
     this.Settings = new Settings(opts?.settingsHost);
+    this.Providers = new Providers(opts?.providersHost);
   }
 }
 
--- packages/plugin-sdk/src/index.ts
+++ packages/plugin-sdk/src/index.ts
@@ -2,4 +2,5 @@ export { NuclearPluginAPI, NuclearAPI } from './api';
 export * from './types';
 export * from './types/settings';
 export * from './types/search';
+export type { ProvidersHost } from './types/providers';
 export { useSetting } from './react/useSetting';
