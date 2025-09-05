764061f7216086b77e3f02778ae7c823a1692810
nukeop <12746779+nukeop@users.noreply.github.com>
2025-09-06 00:20:29 +0200

    Mock framer-motion in ui tests

--- packages/ui/src/test/setup.ts
+++ packages/ui/src/test/setup.ts
@@ -1,5 +1,17 @@
 import '@testing-library/jest-dom';
 
+import { MotionGlobalConfig } from 'framer-motion';
+
 import { setupResizeObserverMock } from './resizeObserverMock';
 
 setupResizeObserverMock();
+
+MotionGlobalConfig.skipAnimations = true;
+MotionGlobalConfig.instantAnimations = true;
+
+vi.mock('framer-motion', async (importOriginal) => {
+  const mod = await importOriginal<typeof import('framer-motion')>();
+  const mockMod = await import('./mockFramerMotion');
+  const factory = mockMod.createFramerMotionMock;
+  return factory(mod);
+});
