74364736bd23eee770d558e43136e736380d06fa
nukeop <12746779+nukeop@users.noreply.github.com>
2025-10-12 13:27:08 +0200

    Add handlers for reloading and removing plugins

--- packages/player/src/views/Plugins/Plugins.tsx
+++ packages/player/src/views/Plugins/Plugins.tsx
@@ -46,6 +46,14 @@ export const Plugins: FC = () => {
                 disabled={!p.enabled}
                 warning={p.warning}
                 warningText={p.warnings.length > 0 ? p.warnings[0] : undefined}
+                onReload={async () => {
+                  if (p.installationMethod === 'dev') {
+                    await store.reloadPlugin(p.metadata.id);
+                  }
+                }}
+                onRemove={async () => {
+                  await store.removePlugin(p.metadata.id);
+                }}
                 rightAccessory={
                   <Toggle
                     data-testid={`toggle-enable-plugin`}
