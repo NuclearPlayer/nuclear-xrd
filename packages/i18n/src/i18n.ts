82bf901fff91310feba94e8615f406d1e544f70c
nukeop <12746779+nukeop@users.noreply.github.com>
2025-10-26 16:58:05 +0100

    Initialize the i18n package

---
+++ packages/i18n/src/i18n.ts
@@ -0,0 +1,24 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import en_US from './locales/en_US.json';
+
+export const resources = {
+  en: en_US,
+} as const;
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'en',
+  fallbackLng: 'en',
+  defaultNS: 'common',
+  ns: ['common', 'navigation', 'search', 'artist'],
+  interpolation: {
+    escapeValue: false,
+  },
+  react: {
+    useSuspense: true,
+  },
+});
+
+export default i18n;
