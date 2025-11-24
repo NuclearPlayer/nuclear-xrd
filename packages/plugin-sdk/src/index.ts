d98fe22cdbe515f08687feae427efc424f2b7659
nukeop <12746779+nukeop@users.noreply.github.com>
2025-11-24 02:45:50 +0100

    Fix exports

--- packages/plugin-sdk/src/index.ts
+++ packages/plugin-sdk/src/index.ts
@@ -5,6 +5,10 @@ export * from './types/search';
 export * from './types/queue';
 export * from './types/streaming';
 export * from './types/metadata';
-export type { ProvidersHost } from './types/providers';
+export type {
+  ProvidersHost,
+  ProviderKind,
+  ProviderDescriptor,
+} from './types/providers';
 export { useSetting } from './react/useSetting';
 export * from '@nuclearplayer/model';
