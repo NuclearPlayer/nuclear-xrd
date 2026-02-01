3b516db9043bdb6f06d61f1616c10e6bbc5d53ce
nukeop <12746779+nukeop@users.noreply.github.com>
2026-02-01 14:53:02 +0100

    First pass website slop

--- packages/website/astro.config.ts
+++ packages/website/astro.config.ts
@@ -1,11 +1,12 @@
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://nuclearplayer.github.io/nuclear-xrd',
   output: 'static',
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
